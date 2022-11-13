032995907f5d435d9ee8b3ee82f0648af3fa101b
LAPTOP-N5A42Q3N\sergi <sergiopinto2501@gmail.com>
2022-11-13 20:20:01 +0100

    no message

---
+++ APPLICAZIONE SERVER/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("APPLICAZIONE SERVER\n");
+    return 0;
+}
---
+++ APPLICZIONE CLIENT/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("APPLICAZIONE CLIENT\n");
+    return 0;
+}
