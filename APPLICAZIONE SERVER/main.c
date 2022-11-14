8e871df1b65e914cb40650b42337fcfbac1633ac
LAPTOP-N5A42Q3N\sergi <sergiopinto2501@gmail.com>
2022-11-14 10:45:54 +0100

    AGGIORNAMENTO SERVER CONNESSIONE AVVENUTA

--- APPLICAZIONE SERVER/main.c
+++ APPLICAZIONE SERVER/main.c
@@ -10,13 +10,30 @@
 //serve per la chiusura
 #endif
 
-#include <stdio.h>
+struct sockaddr_in IndirizzoServer;
+struct sockaddr_in IndirizzoClient;
 
 /*
- * pf -> protocol family, fa parte della famiglia dei protocolli di internet
- * type -> tipo del socket
- *
- */
+struct sockaddr_in {
+    short sin_family;
+    u_short sin_port;
+    struct in_addr sin_addr;
+    char sin_zero[8];
+};
+*/
+void stampaMSGerror(char *error)
+{
+    printf("%s\n",error);
+}
+
+
+#include <stdio.h>
+void PulisciWinSock(){
+    #if defined WIN32
+        WSACleanup();
+    #endif
+}
+
 int main()
 {
     #if defined WIN32
@@ -29,19 +46,55 @@ int main()
         return 0;
     }
     #endif
-    int mysocket;
-
+    //int socket(int pf,int type,int protocol);
     /*
-     * CODICE SERVER
-     */
+    * pf -> protocol family, fa parte della famiglia dei protocolli di internet
+    * type -> tipo del socket
+    * protocol -> protocollo adatto per il tipo di socket
+    */
+    //CREAZIONE SOCKET SERVER
+    int socketServer = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
+    if(socketServer==-1)
+    {
+        printf("ERRORE CREAZIONE SOCKET!\n");
+        PulisciWinSock();
+    }
+    IndirizzoServer.sin_family = AF_INET;
+    IndirizzoServer.sin_addr = ("127.0.0.1"); //localhost ip server
+    IndirizzoServer.sin_port = htons(4999); //indirizzo porta
 
-
-
-
-    closesocket(mysocket);
-    #if defined WIN32
-        WSACleanup();
-    #endif
+    if(bind(socketServer,(struct sockaddr*) &IndirizzoServer,sizeof (IndirizzoServer))<0)
+    {
+        stampaMSGerror("Errore bind()\n");
+        closesocket(socketServer);
+        return -1;
+    }
+    int maxRequ=6;
+    if(listen(socketServer,maxRequ)<0)
+    {
+        stampaMSGerror("Errore listen()\n");
+        closesocket(socketServer);
+        return -1;
+    }
+    //SOCKET SERVER E' LA SOCKET DI BENVENUTO
+    //ORA DOBBIAMO CREARE LE "SOTTO SOCKET"
+    int socketClient;
+    int clientLen;
+    printf("\n\nIn attessa di connessione...");
+    while(1) //lasciale il server in ascolto, quando la connessione con il client sarà finita
+    {
+        clientLen = sizeof(IndirizzoClient);
+        if((socketClient= accept(socketServer,(struct sockaddr *) &IndirizzoClient,&clientLen))<0)
+        {
+            stampaMSGerror("Errore di connessione col client\n");
+            closesocket(socketServer);
+            PulisciWinSock();
+            return -1;
+        }
+        printf("Connessione avvennuta CLIENT: %s\n", inet_ntoa(IndirizzoClient.sin_addr));
+    }
+    closesocket(socketServer);
+    PulisciWinSock();
 
 
     return 0;
